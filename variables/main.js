4d726ffb1be67069b5e4ebccc1123ff06b9c4ed9
Malinina3012 <52998799+Malinina3012@users.noreply.github.com>
2019-11-11 13:52:50 +0500

    Add files via upload

---
+++ variables/main.js
@@ -0,0 +1,19 @@
+//Задача 1
+let algebra = 5;
+let geography = 5;
+let physics = 4;
+let lessons = [algebra, geography, physics];
+let mean = (algebra + geography + physics) / lessons.length
+console.log(mean);
+
+//Задача 2
+let myName = "Natalya";
+let message = ("Привет, мир! Меня зовут " + myName)
+console.log(message);
+
+//Задача 3
+let x = 2;
+let y = 22;
+let z = 0;
+let result = x * y + 5 * z + x - 1;
+console.log(result);
